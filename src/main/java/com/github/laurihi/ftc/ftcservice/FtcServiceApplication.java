b9abdd80d0d0cc2f501238e0a6b4c81913951335
Lauri Hiltunen <hiltusenlauri@gmail.com>
2019-02-11 21:27:37 +0200

    Batman (initial commit)

---
+++ src/main/java/com/github/laurihi/ftc/ftcservice/FtcServiceApplication.java
@@ -0,0 +1,14 @@
+package com.github.laurihi.ftc.ftcservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtcServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FtcServiceApplication.class, args);
+	}
+
+}
+
